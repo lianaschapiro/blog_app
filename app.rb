00b37172f6b148bfb794c9e65e1ec98f0dd0cfc4
lianaschapiro <liana.schapiro@gmail.com>
2016-01-04 18:40:02 -0500

    Fixed indentation

--- app.rb
+++ app.rb
@@ -1,30 +1,29 @@
 require "sinatra"
 require "sinatra/activerecord"
 require "sinatra/flash"
+require "./models"
 
 
 set :database, "sqlite3:myblogdb.sqlite3"
 enable :sessions
 
-require "./models"
+
 
 get '/sign-in' do
-
-		erb :signin
+	erb :signin
 end
 
+
 post '/sign-in' do
-
 	@user = User.where(username: params[:username]).first
-
 	if @user && @user.password == params[:password] 
 		session[:user_id] = @user_id
-		redirect '/'
-    
+		redirect '/'    
 	else
 		redirect '/sign-in'
 	end
 end
+
 
 def current_user
 	if session[:user_id]
@@ -32,8 +31,9 @@ def current_user
 	end
 end
 
+
 get '/logout' do
-	 session.clear
-	 redirect '/sign-in'
-	end
+	session.clear
+	redirect '/sign-in'
+end
 	
